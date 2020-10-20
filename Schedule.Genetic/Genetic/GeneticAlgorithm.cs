884a99dea5bab15c116a54094503421c1d12162f
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-10-20 23:47:19 +0300

    Created genetic algorithm project

---
+++ Schedule.Genetic/Genetic/GeneticAlgorithm.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Schedule.Genetic.Genetic
+{
+    public class GeneticAlgorithm<A>
+    {
+        private int _individualLength;
+        private List<A> _alphabet;
+        private double _probability;
+    }
+}
