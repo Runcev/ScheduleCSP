c547d6d14c4e16602811c37e20061e4a8093c9ef
Віталій Кенийз <vitalii.kenyiz@ukma.edu.ua>
2020-10-21 13:26:23 +0300

    Add algorithm

---
+++ Schedule.Genetic/Genetic/Individual.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Schedule.Genetic.Genetic
+{
+    public class Individual<A>
+    {
+        public List<A> Representation { get; }
+        
+        public Individual(List<A> representation)
+        {
+            Representation = representation;
+        }
+
+        public int Length()
+        {
+            return Representation.Count;
+        }
+    }
+}
