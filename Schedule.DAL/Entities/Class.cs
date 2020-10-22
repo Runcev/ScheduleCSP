88b6f0a5a69afc42df5dad78eab2ad3f19c261f4
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-10-23 01:01:44 +0300

    Added one-to-one relation between group and class

--- Schedule.DAL/Entities/Class.cs
+++ Schedule.DAL/Entities/Class.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using Schedule.DAL.Entities.Common;
 using Schedule.DAL.Enums;
 
@@ -7,11 +5,6 @@ namespace Schedule.DAL.Entities
 {
     public class Class : Entity
     {
-        public Class()
-        {
-            
-        }
-        
         public ClassType Type { get; set; }
         
         public int DayTimeId { get; set; }
@@ -25,29 +18,7 @@ namespace Schedule.DAL.Entities
         
         public int SubjectId { get; set; }
         public Subject Subject { get; set; }
-
-        private List<Group> _groups = new List<Group>();
         
-        public List<Group> Groups
-        {
-            get => _groups;
-            set
-            {
-                var groups = value;
-                
-                int studentCount = Subject.Specialty.StudentCount;
-                int studentsInOneGroup = studentCount / groups.Count;
-                int studentsRemains = studentCount % groups.Count;
-
-                foreach (var group in groups)
-                {
-                    group.Count = studentsInOneGroup;
-                }
-
-                groups.First().Count += studentsRemains;
-
-                _groups = groups;
-            }
-        }
+        public Group Group { get; set; }
     }
 }
