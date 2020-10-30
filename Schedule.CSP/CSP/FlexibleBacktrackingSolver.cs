e9a68ffd469e45fc26dd184368ed9c5479d55ca2
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-10-31 01:24:18 +0200

    implemented backtracking solver

--- Schedule.CSP/CSP/FlexibleBacktrackingSolver.cs
+++ Schedule.CSP/CSP/FlexibleBacktrackingSolver.cs
@@ -6,36 +6,32 @@ using Schedule.CSP.Inference;
 
 namespace Schedule.CSP.CSP
 {
-    public class FlexibleBacktrackingSolver<Var, Val> : AbstractBacktrackingSolver<Var, Val> where Var : Variable
+    public class FlexibleBacktrackingSolver<Var, Val> where Var : Variable
     {
         private CspHeuristics.IVariableSelectionStrategy<Var, Val> VariableSelectionStrategy { get; set; }
         private CspHeuristics.IValueOrderingStrategy<Var, Val> ValueOrderingStrategy { get; set; }
         private IInferenceStrategy<Var, Val> InferenceStrategy { get; set; }
-
-
-
+        
         public Assignment<Var, Val> Solve(CSP<Var, Val> csp)
         {
             if (InferenceStrategy != null)
             {
-                csp = csp.CopyDomains();
-                InferenceLog log = InferenceStrategy.Apply(csp);
+                var log = InferenceStrategy.Apply(csp);
                 if (!log.IsEmpty())
                 {
-                    fireStateChanged(csp, null, null);
-                    if (log.InConsistencyFound())
+                    if (log.IsConsistencyFound())
                     {
-                        return Empty();
+                        return null;
                     }
                 }
             }
 
-            return Solve(csp);
+            return null;
         }
 
-        public Var SelectUnassignedVariable(CSP<Var, Val> csp, Assignment<Var, Val> assigment)
+        private Var SelectUnassignedVariable(CSP<Var, Val> csp, Assignment<Var, Val> assigment)
         {
-            IEnumerable<Var> vars = csp.GetVariables.Where(v => !assigment.Contains(v));
+            var vars = csp.Variables.Where(v => !assigment.Contains(v));
             if (VariableSelectionStrategy != null)
             {
                 vars = VariableSelectionStrategy.Apply(csp, vars);
@@ -43,18 +39,18 @@ namespace Schedule.CSP.CSP
             return vars.ElementAt(0);
         }
 
-        protected IEnumerable<Val> OrderDomainValues(CSP<Var, Val> csp, Assignment<Var, Val> assignment, Var var)
+        private IEnumerable<Val> OrderDomainValues(CSP<Var, Val> csp, Assignment<Var, Val> assignment, Var var)
         {
             return (ValueOrderingStrategy != null)
                 ? ValueOrderingStrategy.Apply(csp, assignment, var)
                 : csp.GetDomain(var);
         }
 
-        protected IInferenceLog<Var, Val> Inference(CSP<Var, Val> csp, Assignment<Var, Val> assignment, Var var)
+        private IInferenceLog<Var, Val> Inference(CSP<Var, Val> csp, Assignment<Var, Val> assignment, Var var)
         {
             return (InferenceStrategy != null)
                 ? InferenceStrategy.Apply(csp, assignment, var)
-                : IInferenceLog.EmptyLog();
+                : new EmptyLog<Var, Val>();
         }
         
     }
