95c155396f6c12b7bd31cfd7f6437a507c5d71f3
Віталій Кенийз <vitalii.kenyiz@ukma.edu.ua>
2020-10-30 16:20:40 +0200

    create Assigment

--- Schedule.CSP/CSP/Assignment.cs
+++ Schedule.CSP/CSP/Assignment.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace Schedule.CSP.CSP
 {
@@ -9,7 +11,45 @@ namespace Schedule.CSP.CSP
 
         public IEnumerable<Var> GetVariables()
         {
-            throw new NotImplementedException();
+            return new List<Var>(_variableToValueMap.Keys);
+        }
+
+        public Val GetValue(Var var)
+        {
+            return _variableToValueMap[var];
+        }
+
+        public void Add(Var var, Val value)
+        {
+            if (value != null)
+            {
+                _variableToValueMap.Add(var, value);
+            }
+        }
+
+        public void Remove(Var var)
+        {
+            _variableToValueMap.Remove(var);
+        }
+
+        public bool Contains(Var var)
+        {
+            return _variableToValueMap.ContainsKey(var);
+        }
+
+        public bool IsConsistent(IEnumerable<IConstraint<Var, Val>> constraints)
+        {
+            return constraints.All(c => c.isSatisfiedWith(this));
+        }
+
+        public bool IsComplete(IEnumerable<Var> vars)
+        {
+            return vars.All(Contains);
+        }
+
+        public bool IsSolution(CSP<Var, Val> csp)
+        {
+            return IsConsistent(csp.GetContraints()) && IsComplete(csp.GetVariables());
         }
     }
 }
